ab0aef5377780e9e7d181e7a9dae95e61e37ea11
Rüdiger zu Dohna <snackbox@sinntr.eu>
2015-03-01 15:59:53 +0100

    project structure cleanup; updated to 1.0-SNAPSHOT

---
+++ cdi-config-tck/src/main/java/com/github/t1/config/ConfiguredResource.java
@@ -0,0 +1,17 @@
+package com.github.t1.config;
+
+import static javax.ws.rs.core.MediaType.*;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.Response;
+
+@Path("/configured")
+public class ConfiguredResource {
+    @Config(name = "config-key")
+    String value;
+
+    @GET
+    public Response getConfig() {
+        return Response.ok("[" + value + "]\n", TEXT_PLAIN).build();
+    }
+}
